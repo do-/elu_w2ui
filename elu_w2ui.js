935715851aab6c19b3124982ce171f3814ae768c
Aleksandr Dmitriev <admitriev@cheby.ru>
2018-10-26 17:38:31 +0300

    Key recid can be alredy exist for dia2w2uiRecords

--- elu_w2ui.js
+++ elu_w2ui.js
@@ -24,7 +24,7 @@ function dia2w2uiRecords (records) {
 
     $.each (records, function () {
 
-        this.recid = this.id
+        if (!this.recid) this.recid = this.id
 
         if (this._status) {
 
