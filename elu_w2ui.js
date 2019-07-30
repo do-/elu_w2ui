94ed853baffda34a56a96a2eacf305aa96b35eb3
Aleksandr Dmitriev <admitriev@cheby.ru>
2019-07-30 10:12:57 +0300

    Fix col attribute for render

--- elu_w2ui.js
+++ elu_w2ui.js
@@ -519,7 +519,7 @@ w2obj.grid.prototype.toArray = function (iterator_cb, done_cb) {
             var keys   = Object.keys(row)
             var values = []
 
-            keys.forEach(function(key, col) {
+            keys.forEach(function(key) {
 
                 var value = row[key]
 
@@ -533,7 +533,7 @@ w2obj.grid.prototype.toArray = function (iterator_cb, done_cb) {
 
             })
 
-            fields_names.forEach(function(key) {
+            fields_names.forEach(function(key, col) {
 
                 var field = fields.find(function(i) { return i.name === key })
                 var value = row[key]
